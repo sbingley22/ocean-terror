64da9ae8fad0efe605ede8b22bc5000fa0757b7e
Sean B <seanb4@hotmail.co.uk>
2024-06-11 08:43:23 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ocean Terror
+
+Live Link: https://sbingley22.github.io/ocean-terror/
+
+Navigate the terrorfiying stormy ocean.
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS with Vite + React!</h1>
+      <p className="text-green-200">HHH</p>
+    </div>
+  )
+}
+
+export default App
